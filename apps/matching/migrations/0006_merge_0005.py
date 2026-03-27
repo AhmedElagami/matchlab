9ff6c5f9ef76eab6b420e8fb5ca925e8e2c7a90e
Khongorzul Khenchbish <khongorzul.khenchbish@ericsson.com>
2026-03-27 01:22:03 +0100

    ci: merge conflicting 0005 migrations

---
+++ apps/matching/migrations/0006_merge_0005.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('matching', '0005_matchrun_base_match_run_alter_matchrun_mode'),
+        ('matching', '0005_simplify_profiles'),
+    ]
+
+    operations = []
